1b773b210602c142798366c891263294030bc710
Tom Dale <thomas.dale2@gmail.com>
2022-06-02 14:59:29 -0700

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,7 +9,7 @@
       </div>
       <div class="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
         <h1 class="text-4xl font-extrabold tracking-tight text-white md:text-5xl lg:text-6xl">Far Far Away News</h1>
-        <p class="mt-6 max-w-3xl text-xl text-gray-300">A Demo Site for Vue Conf 2022. Site moved from <a href="https://vueconf.flywheelsites.com/">Wordpress</a> to Vue 3 to improve performance. This version has focused on using a video format for animated content. You can compare to the previous <a href="https://vueconf22.vercel.app/">Vue Version</a> to see the lower score using an animated GIF.</p>
+        <p class="mt-6 max-w-3xl text-xl text-gray-300">A Demo Site for Vue Conf 2022. Site moved from <a href="https://vueconf.flywheelsites.com/">Wordpress</a> to Vue 3 to improve performance. This version has focused on optimizing image with next-gen formats. You can compare to the previous <a href="https://vueconf-a.vercel.app/">Vue Version</a> to see the lower score using standard JPGs.</p>
       </div>
     </div>
 
